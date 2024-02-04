906f25060f86f6f786d7407d84d1359f961d42ea
Kartik Kittad <99243266+Kartikkittad@users.noreply.github.com>
2024-02-04 21:09:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,4 @@ export default defineConfig({
   },
 })
 
-const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
