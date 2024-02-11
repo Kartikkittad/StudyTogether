cbde91bedfd8950eec9e9edf45462626132413b1
Kartik Kittad <99243266+Kartikkittad@users.noreply.github.com>
2024-02-11 16:36:32 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { ClerkProvider } from '@clerk/clerk-react';
 import App from './App';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = 'pk_test_aW5zcGlyZWQtYmFib29uLTAuY2xlcmsuYWNjb3VudHMuZGV2JA';
 
 if (!PUBLISHABLE_KEY) {
     throw new Error("Missing Publishable Key")
 }
 
 ReactDOM.render(
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey='pk_test_aW5zcGlyZWQtYmFib29uLTAuY2xlcmsuYWNjb3VudHMuZGV2JA'>
         <App />
     </ClerkProvider>,
     document.getElementById('root')
